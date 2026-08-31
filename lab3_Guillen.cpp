875c022d1ac95ea197e8942845544bfdbfcae4ad
Dana Yajanis Guillén Trejos <dana.guillen.trejos@est.una.ac.cr>
2026-08-31 09:09:23 -0600

    Agregados metodos de deposito, retiro y getters

--- lab3_Guillen.cpp
+++ lab3_Guillen.cpp
@@ -15,11 +15,44 @@ public:
         this->balance = balance;
         totalAccounts = totalAccounts + 1;
     }
+
+    void deposit(double amount) {
+        if (amount > 0) {
+            balance = balance + amount;
+        }
+        else {
+            cout << "Error en deposito" << endl;
+        }
+    }
+
+    bool withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance = balance - amount;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    double getBalance() const {
+        return balance;
+    }
+
+    string getOwner() const {
+        return owner;
+    }
 };
 
 int Account::totalAccounts = 0;
 
 int main() {
     Account c1("Juan", 1000);
+    c1.deposit(200);
+
+    if (c1.withdraw(500)) {
+        cout << "Retiro exitoso. Saldo: " << c1.getBalance() << endl;
+    }
+
     return 0;
 }
